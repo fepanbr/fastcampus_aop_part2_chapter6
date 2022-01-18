2ad4c235ac8398c0d2e4e8bc7a6d5bbfbe11a0f5
fepanbr <fepanbr@gmail.com>
2022-01-18 23:37:36 +0900

    initialize

---
+++ app/src/main/java/fastcampus/aop/part2/chapter06/MainActivity.kt
@@ -0,0 +1,51 @@
+package fastcampus.aop.part2.chapter06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindView()
+    }
+
+    private fun bindView() {
+        seekBar
+            .setOnSeekBarChangeListener(
+                object: SeekBar.OnSeekBarChangeListener {
+                    override fun onProgressChanged(
+                        seekBar: SeekBar?,
+                        progress: Int,
+                        fromUser: Boolean
+                    ) {
+                        remainMinutesTextView.text = "%02d".format(progress)
+                    }
+
+                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                    }
+
+                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                    }
+                }
+            )
+    }
+}
